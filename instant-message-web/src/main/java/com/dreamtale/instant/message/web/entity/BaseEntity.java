b31482dbfa929b1e1a6bc8519c5d90c15ac48a33
yingshuai <yingshuai>
2018-08-06 17:22:09 +0800

    删除部分无用代码 增加实例

--- instant-message-web/src/main/java/com/dreamtale/instant/message/web/entity/BaseEntity.java
+++
@@ -1,6 +0,0 @@
-package com.dreamtale.instant.message.web.entity;
-
-import java.io.Serializable;
-
-public class BaseEntity implements Serializable {
-}
--- instant-message-web/src/main/java/com/dreamtale/instant/message/web/resource/ReceiveMessageResource.java
+++ instant-message-web/src/main/java/com/dreamtale/instant/message/web/resource/ReceiveMessageResource.java
@@ -1,28 +1,28 @@
-//package com.dreamtale.dreamtale.instant.message.resource;
-//
-//import MessageEntity;
-//import com.dreamtale.dreamtale.instant.message.service.ReceiveMessageService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/receive/message")
-//public class ReceiveMessageResource {
-//
-//    @Autowired
-//    ReceiveMessageService receiveMessageService;
-//
-//    @RequestMapping("/receive")
-//    public String receiveMessage(MessageEntity messageEntity){
-//        return receiveMessageService.getMessage(messageEntity);
-//    }
-//
-//    @RequestMapping("/push")
-//    public String pushMessage(){
-//        return "你是不是傻！";
-//    }
-//
-//
-//
-//}
+package com.dreamtale.instant.message.web.resource;
+
+import com.dreamtale.instant.message.api.entity.message.MessageEntity;
+import com.dreamtale.instant.message.web.service.ReceiveMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/receive/message")
+public class ReceiveMessageResource {
+
+    @Autowired
+    ReceiveMessageService receiveMessageService;
+
+    @RequestMapping("/receive")
+    public String receiveMessage(MessageEntity messageEntity){
+        return receiveMessageService.getMessage(messageEntity);
+    }
+
+    @RequestMapping("/push")
+    public String pushMessage(){
+        return "你是不是傻！";
+    }
+
+
+
+}
