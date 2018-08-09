c47c866db88fadaf179cce3f30a2a0c11189a90a
yingshuai <865887234@qq.com>
2018-08-09 23:19:04 +0800

    入库操作

--- instant-message-web/src/main/java/com/dreamtale/instant/message/web/resource/ProprietorResource.java
+++ instant-message-web/src/main/java/com/dreamtale/instant/message/web/resource/ProprietorResource.java
@@ -3,20 +3,26 @@ package com.dreamtale.instant.message.web.resource;
 import com.dreamtale.instant.message.web.service.ProprietorService;
 import org.jfree.data.json.impl.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * 业主功能相关
  * Created by yingshuai on 2018/8/9.
  */
-@Path("/proprietor")
+@RestController
+@RequestMapping("/proprietor")
+@Produces(MediaType.APPLICATION_JSON)
 public class ProprietorResource {
 
     @Autowired
     ProprietorService proprietorService;
 
-    @Path("/bachInsertProprietor")
+    @GetMapping("/bachInsertProprietor")
     public JSONObject batchInsertProprietor(){
         return proprietorService.batchInsertProprietor();
     }
--- instant-message-web/src/main/java/com/dreamtale/instant/message/web/service/ProprietorService.java
+++ instant-message-web/src/main/java/com/dreamtale/instant/message/web/service/ProprietorService.java
@@ -6,6 +6,8 @@ import org.jfree.data.json.impl.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 /**
  * Created by yingshuai on 2018/8/9.
  */
@@ -16,7 +18,19 @@ public class ProprietorService {
     BaseUserMapper baseUserMapper;
 
     public JSONObject batchInsertProprietor(){
-        baseUserMapper.insert(new BaseUser());
+        BaseUser baseUser = new BaseUser();
+        baseUser.setCreateTime(new Date());
+        baseUser.setEmail("865887234@qq.com");
+        baseUser.setMobile("18610311785");
+        baseUser.setOpenId("dream");
+        baseUser.setRealNameCheckStatus(1);
+        baseUser.setEmailCheckStatus(1);
+        baseUser.setStatus(1);
+        baseUser.setUserId("10000");
+        baseUser.setUserName("dreamtale");
+        baseUser.setUserNickName("梦");
+        baseUser.setUserPassword("123456");
+        baseUserMapper.insert(baseUser);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("flag",true);
         jsonObject.put("message", "批量导入成功！");
